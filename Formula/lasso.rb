5b49b133bed2b858104f76abb0e70fe64f4d3c2c
Kyle Browning <kylebrowning@me.com>
2026-03-05 06:55:32 -0800

    Update lasso to 0.2.2

--- Formula/lasso.rb
+++ Formula/lasso.rb
@@ -1,9 +1,9 @@
 class Lasso < Formula
   desc "CLI and CI integration platform for iOS Simulator automation"
   homepage "https://github.com/kylebrowning/lasso"
-  url "https://github.com/kylebrowning/lasso/releases/download/0.2.0/lasso-0.2.0-macos.tar.gz"
-  sha256 "7710e19727fcfc8fbe5a6b5c1ed7057ce91d8197025603e007c93f3cdcfab76e"
-  version "0.2.0"
+  url "https://github.com/kylebrowning/lasso/releases/download/0.2.2/lasso-0.2.2-macos.tar.gz"
+  sha256 "d31bf78135d3ab7d393b05c31cab64d4b8a168b08ab631eb45bdfbbdbce83835"
+  version "0.2.2"
   license "MIT"
 
   depends_on :macos
